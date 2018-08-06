d17a8668242a0767d795458d81fde8382cc44e51
Caitlin <caitlinchou@gmail.com>
2018-08-05 20:43:14 -0400

    export fixes #7

---
+++ menu/export.py
@@ -0,0 +1,109 @@
+from django.http import HttpResponse
+from openpyxl import Workbook
+
+from .models import *
+
+
+def export(request):
+    # each item is a line in the excel sheet
+    # it can be anything you pull from the db
+    rest_id = request.user.restaurante.id
+    wb = Workbook(write_only=True)
+
+    proveedores = Proveedor.objects.filter(restaurante_id=rest_id)
+    ws_prov = wb.create_sheet(title='Proveedores')
+    ws_prov.append(['Nombre',
+                    'Telefono',
+                    'Representante',
+                    'Telefono de Representante',
+                    'Correo Electronico'])
+    for proveedor in proveedores:
+        ws_prov.append([
+            proveedor.nombre,
+            proveedor.telefono,
+            proveedor.representante,
+            proveedor.telefono_de_representante,
+            proveedor.correo_electronico])
+
+    ingredientes = Ingrediente.objects.filter(restaurante_id=rest_id)
+    ws_ing = wb.create_sheet(title='Ingredientes')
+    ws_ing.append([
+        'Nombre',
+        'Marca',
+        'Proveedor',
+        'Costo ($)',
+        'Medida',
+        'Cantidad',
+        'Costo Unitario ($)'])
+    for ing in ingredientes:
+        ws_ing.append([
+            ing.nombre,
+            ing.marca,
+            ing.proveedor.nombre,
+            ing.costo,
+            ing.medida,
+            ing.cantidad,
+            ing.unit_cost
+        ])
+
+    recetas = Receta.objects.filter(restaurante_id=rest_id)
+    ws_rec = wb.create_sheet(title='Recetas')
+    ws_rec.append([
+        'Nombre',
+        'Costo ($)',
+        'Cantidad',
+        'Medida',
+        'Costo Unitario ($)'
+    ])
+    for rec in recetas:
+        ws_rec.append([
+            rec.nombre,
+            rec.costo,
+            rec.cantidad,
+            rec.medida,
+            rec.unit_cost
+        ])
+
+    platillos = Platillo.objects.filter(restaurante_id=rest_id, bebida=False)
+    ws_plat = wb.create_sheet(title='Platillos')
+    ws_plat.append([
+        'Nombre',
+        'Costo ($)',
+        'Precio ($)',
+        'Costo / Precio (%)',
+        'Ganancia ($)'
+    ])
+    for plat in platillos:
+        ws_plat.append([
+            plat.nombre,
+            plat.costo,
+            plat.precio,
+            plat.costo_percentaje,
+            plat.ganancia
+        ])
+
+    bebidas = Platillo.objects.filter(restaurante_id=rest_id, bebida=True)
+    ws_beb = wb.create_sheet(title='Bebidas')
+    ws_beb.append([
+        'Nombre',
+        'Costo ($)',
+        'Precio ($)',
+        'Costo / Precio (%)',
+        'Ganancia ($)'
+    ])
+    for beb in bebidas:
+        ws_plat.append([
+            beb.nombre,
+            beb.costo,
+            beb.precio,
+            beb.costo_percentaje,
+            beb.ganancia
+        ])
+
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=miel-tech.xlsx'
+
+    wb.save(response)
+
+    return response
--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from . import views
+from . import export
 
 app_name = "menu"
 
@@ -11,12 +12,14 @@ urlpatterns = [
     url(r'^proveedores/anadir/$', views.add_proveedor, name='proveedores_add'),
     url(r'^platillos/$', views.platillos, name='platillos'),
     url(r'^platillos/suprimir/$', views.del_platillo, name='platillos_delete'),
+    url(r'^platillos/exportar/$', export.export, name='platillos_export'),
     url(r'^bebidas/$', views.bebidas, name='bebidas'),
     url(r'^ingredientes/$', views.ingredientes, name='ingredientes'),
-    url(r'^ingredientes/$', views.ingredientes, name='ingredientes_sort'), #TODO change this
     url(r'^ingredientes/suprimir/$', views.del_ingredient, name='ingredientes_delete'),
+    url(r'^ingredientes/exportar/$', export.export, name='ingredientes_export'),
     url(r'^recetas/$', views.recetas, name='recetas'),
     url(r'^recetas//?(?P<pk>[-\w\d]+)/?$', views.RecetaUpdateView.as_view(), name='recetas_view'),
     url(r'^recetas/suprimir/$', views.del_receta, name='recetas_delete'),
     url(r'^recetas/cambiar//?(?P<pk>[-\w\d]+)/?$', views.edit_receta, name='recetas_edit'),
+    url(r'^recetas/exportar/$', export.export, name='recetas_export'),
 ]
